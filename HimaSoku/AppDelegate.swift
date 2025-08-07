84c063c9b00792119be60e5ac81e067ff7391294
sawakishuto <swkshuto0208@icloud.com>
2025-08-08 04:18:42 +0900

    first commit

---
+++ HimaSoku/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  HimaSoku
+//
+//  Created by Shuto Sawaki on 2025/08/08.
+//
+
+import Foundation
---
+++ HimaSoku/GroupMembersView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupMemberView.swift
+//  HimaSoku
+//
+//  Created by Shuto Sawaki on 2025/08/08.
+//
+
+import SwiftUI
+
+struct GroupMemberView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupMemberView()
+}
